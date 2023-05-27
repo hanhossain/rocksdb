2dd923a6795854cb9b01492a9a8d782c10fb2e04
Han Hossain <hanhossain@outlook.com>
2023-05-27 01:51:00 -0700

    convert rust math funcs into macro

--- rocksdb-rs/src/math.rs
+++ rocksdb-rs/src/math.rs
@@ -1,63 +1,24 @@
-pub fn floor_log2_i8(v: i8) -> i32 {
-    v.ilog2() as i32
+use paste::paste;
+
+macro_rules! gen_math {
+    ($id:ident) => {
+        paste! {
+            pub fn [<floor_log2_$id>](v: $id) -> i32 {
+                v.ilog2() as i32
+            }
+
+            pub fn [<trailing_zeros_$id>](v: $id) -> i32 {
+                v.trailing_zeros() as i32
+            }
+        }
+    };
 }
 
-pub fn floor_log2_i16(v: i16) -> i32 {
-    v.ilog2() as i32
-}
-
-pub fn floor_log2_i32(v: i32) -> i32 {
-    v.ilog2() as i32
-}
-
-pub fn floor_log2_i64(v: i64) -> i32 {
-    v.ilog2() as i32
-}
-
-pub fn floor_log2_u8(v: u8) -> i32 {
-    v.ilog2() as i32
-}
-
-pub fn floor_log2_u16(v: u16) -> i32 {
-    v.ilog2() as i32
-}
-
-pub fn floor_log2_u32(v: u32) -> i32 {
-    v.ilog2() as i32
-}
-
-pub fn floor_log2_u64(v: u64) -> i32 {
-    v.ilog2() as i32
-}
-
-pub fn trailing_zeros_i8(v: i8) -> i32 {
-    v.trailing_zeros() as i32
-}
-
-pub fn trailing_zeros_i16(v: i16) -> i32 {
-    v.trailing_zeros() as i32
-}
-
-pub fn trailing_zeros_i32(v: i32) -> i32 {
-    v.trailing_zeros() as i32
-}
-
-pub fn trailing_zeros_i64(v: i64) -> i32 {
-    v.trailing_zeros() as i32
-}
-
-pub fn trailing_zeros_u8(v: u8) -> i32 {
-    v.trailing_zeros() as i32
-}
-
-pub fn trailing_zeros_u16(v: u16) -> i32 {
-    v.trailing_zeros() as i32
-}
-
-pub fn trailing_zeros_u32(v: u32) -> i32 {
-    v.trailing_zeros() as i32
-}
-
-pub fn trailing_zeros_u64(v: u64) -> i32 {
-    v.trailing_zeros() as i32
-}
+gen_math!(i8);
+gen_math!(i16);
+gen_math!(i32);
+gen_math!(i64);
+gen_math!(u8);
+gen_math!(u16);
+gen_math!(u32);
+gen_math!(u64);
