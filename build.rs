18f147cf6acb578b6cbee7ccea3e176d1074767d
Han Hossain <hanhossain@outlook.com>
2023-06-25 01:06:33 -0700

    use autocxx

--- build.rs
+++ build.rs
@@ -1,20 +1,22 @@
 use cmake::Config;
 
-fn main() {
-    println!("cargo:rerun-if-changed=rocksdb-cxx");
-    println!("cargo:rerun-if-changed=src/lib.rs");
-
+fn main() -> miette::Result<()> {
     let dst = Config::new("rocksdb-cxx")
         .define("WITH_GFLAGS", "OFF")
         .generator("Ninja")
         .build_target("rocksdb")
         .build();
 
-    cxx_build::bridge("src/lib.rs")
+    autocxx_build::Builder::new("src/lib.rs", &["rocksdb-cxx"])
+        .build()?
         .include("rocksdb-cxx/include")
         .flag_if_supported("-std=c++17")
         .compile("rocksdb-cxx-cxx");
 
+    println!("cargo:rerun-if-changed=rocksdb-cxx");
+    println!("cargo:rerun-if-changed=src/lib.rs");
+
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-lib=static=rocksdb");
+    Ok(())
 }
